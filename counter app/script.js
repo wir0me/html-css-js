d74febf3f204a31a37806c518b5bca55f0a197e9
wir0me <117565638+wir0me@users.noreply.github.com>
2022-12-29 15:15:28 +0800

    Updated counter app
    - added an option for user input;
    - updated the javascript (used the switch statement instead of function for each operation.

--- counter app/script.js
+++ counter app/script.js
@@ -1,21 +1,38 @@
 let count = 0;
 
-function addOne() {
-  count += 1;
+
+function doMath(operation) {
+  switch (operation) {
+    case '+':
+      count += 1;
+      break;
+    case '-':
+      count -= 1;
+      break;
+  };
   render();
 };
+
+function doUserMath(operation) {
+  let inputCount = Number(document.getElementById('inputCount').value);
+  switch (operation) {
+    case '+':
+      count += inputCount;
+      break;
+    case '-':
+      count -= inputCount;
+      break;
+  };
+
+  render();
+}
 
 function resetCount() {
   count = 0;
   render();
 };
 
-function minusOne() {
-  count -= 1;
-  render();
-}
-
 function render() {
   document.getElementById("count").innerHTML = count;
-};
-
+  document.getElementById('inputCount').value = '';
+}
