b31b6ffcd890205a26b3ccf90f40efcee7f8713a
wir0me <117565638+wir0me@users.noreply.github.com>
2023-01-02 16:25:21 +0800

    Updated Gradient Generator
    - added a 'lock color' feature

--- color randomizer/scripts/color.js
+++ color randomizer/scripts/color.js
@@ -30,32 +30,64 @@ function generateHexCode() {
 }
 
 
+// lock and unlock colors
+const lockToggle1 = document.getElementsByClassName("lock-button")[0];
+let colorLocked1 = false;
+
+const lockToggle2 = document.getElementsByClassName("lock-button")[1];
+let colorLocked2 = false;
+
+lockToggle1.addEventListener("click", lockColor1);
+function lockColor1() {
+  if (colorLocked1 === true) {
+    colorLocked1 = false;
+    lockToggle1.style.color = 'var(--greyed)';
+  } else {
+    colorLocked1 = true;
+    lockToggle1.style.color = 'var(--green)';
+  }
+}
+
+lockToggle2.addEventListener("click", lockColor2);
+function lockColor2() {
+  if (colorLocked2 === true) {
+    colorLocked2 = false;
+    lockToggle2.style.color = 'var(--greyed)';
+  } else {
+    colorLocked2 = true;
+    lockToggle2.style.color = 'var(--green)';
+  }
+}
+
 
 
 // gradient colors
 const generateButton = document.getElementById("generate-button");
+let firstHexCode
+let secondHexCode
 
 generateButton.addEventListener('click', generateHexCodes);
 function generateHexCodes() {
-  let firstHexCode = "#";
-  let secondHexCode = "#";
+  firstHexCode = "#";
+  secondHexCode = "#";
 
-  for (i = 0; i < 6; i++) {
-    var char = hexCodeChar[Math.floor(Math.random() * hexCodeChar.length)];
-    firstHexCode += char;
+  if (colorLocked1 === false) {
+    for (i = 0; i < 6; i++) {
+      var char = hexCodeChar[Math.floor(Math.random() * hexCodeChar.length)];
+      firstHexCode += char;
+    }
+    document.getElementsByClassName("hex-code")[0].innerHTML = firstHexCode;
+    document.documentElement.style.setProperty("--gradient1", firstHexCode);
   }
 
-  document.getElementsByClassName("hex-code")[0].innerHTML = firstHexCode;
-  document.documentElement.style.setProperty("--gradient1", firstHexCode);
-
-  for (i = 0; i < 6; i++) {
-    var char = hexCodeChar[Math.floor(Math.random() * hexCodeChar.length)];
-    secondHexCode += char;
+  if (colorLocked2 === false) {
+    for (i = 0; i < 6; i++) {
+      var char = hexCodeChar[Math.floor(Math.random() * hexCodeChar.length)];
+      secondHexCode += char;
+    }
+    document.getElementsByClassName("hex-code")[1].innerHTML = secondHexCode;
+    document.documentElement.style.setProperty("--gradient2", secondHexCode);
   }
-
-  document.getElementsByClassName("hex-code")[1].innerHTML = secondHexCode;
-  document.documentElement.style.setProperty("--gradient2", secondHexCode);
-
-
 }
+
 
