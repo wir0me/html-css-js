0e6d3c9cd28d80f14dad48d0d6b96a5e552329bc
wir0me <117565638+wir0me@users.noreply.github.com>
2023-02-01 14:30:27 +0800

    updated unit converter
    - started the dropdown menu for units
    - finished the conversion function for celsius to fahrenheit
    - fixed paddings for small mobile screens

--- unit converter/script.js
+++ unit converter/script.js
@@ -1,22 +1,42 @@
+// conversion values
 
-// conversion
 
-const userInput = document.querySelector('#user-input');
-const resultContainer = document.querySelector('.to-unit input');
-
-// const inputHandler = function(e) {
-//   resultContainer.innerHTML = e.target.value;
-// }
-
-// userInput.addEventListener('input', inputHandler);
 
 // ─────────────────────────────────────────────────────────────────────────────
 
-const source = document.getElementById('source');
-const result = document.getElementById('result');
+// conversion
 
-const inputHandler = function(e) {
-  result.innerHTML = e.target.value;
+const userInput = document.querySelector('input[type="number"]');
+const resultContainer = document.querySelector('.to-unit input');
+
+userInput.addEventListener('keyup', unitConvert);
+
+// ─────────────────────────────────────────────────────────────────────────────
+
+function unitConvert(e) {
+  let freezingPointInF = 32;
+  let result = (e.target.value * (9/5)) + freezingPointInF;
+  resultContainer.value = result;
 }
 
-source.addEventListener('input', inputHandler);
+// ─────────────────────────────────────────────────────────────────────────────
+
+// dropdown
+
+const unitDropdown = document.getElementsByClassName('select-unit');
+const dropdownOptions = document.getElementsByClassName('unit-select-options');
+const dropdownFrom = document.getElementsByClassName('from')[0];
+const dropdownTo = document.getElementsByClassName('to')[0];
+
+for (let i = 0; i < dropdownOptions.length; i++) {
+  unitDropdown[i].addEventListener('click', function() {
+    if (i == 0) {
+      dropdownFrom.style.display = 'contents';
+      dropdownTo.style.display = 'none';
+
+    }
+  });
+}
+
+// ─────────────────────────────────────────────────────────────────────────────
+
